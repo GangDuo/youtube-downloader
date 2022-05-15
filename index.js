c49f0eecdf7a94dd0cfdcb70ba700cb3494061f1
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2022-05-15 21:35:30 +0900

    mp4をmp3へ変換する。

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+const { exec } = require('child_process');
 
 const YID = '';
 
-ytdl(`http://www.youtube.com/watch?v=${YID}`)
-  .pipe(fs.createWriteStream(`${YID}.mp4`));
+const video = ytdl(`http://www.youtube.com/watch?v=${YID}`);
+video.pipe(fs.createWriteStream(`${YID}.mp4`));
+video.on('end', () => {
+  exec(`ffmpeg -y -i ${YID}.mp4 ${YID}.mp3`, (error, stdout, stderr) => {
+    if(error) {
+      console.error(error)
+      return
+    }
+    console.log(stdout)
+    console.log(stderr)
+  })
+})
